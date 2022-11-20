17964552ee867cc51559213e87e28bbfba24fd4d
hirenbioinfo <hiren.ghosh@gmail.com>
2022-11-20 14:55:06 +0100

    EnsDb.Hsapiens.v79 added

--- Ensembl2genesymbol.R
+++ Ensembl2genesymbol.R
@@ -26,3 +26,22 @@ infile %>%
   dplyr::arrange(padj) %>% 
   head(20) %>% 
   dplyr::inner_join(grch38, by = c("ENSEMBL" = "ensgene"))
+
+
+
+
+###
+# Install the package if you have not installed by running this command: 
+BiocManager::install("EnsDb.Hsapiens.v79")
+
+library(EnsDb.Hsapiens.v79)
+
+# 1. Convert from ensembl.gene to gene.symbol
+ensembl.genes <- c("ENSG00000150676", "ENSG00000099308", "ENSG00000142676", "ENSG00000180776", "ENSG00000108848", "ENSG00000277370", "ENSG00000103811", "ENSG00000101473")
+
+geneIDs1 <- ensembldb::select(EnsDb.Hsapiens.v79, keys= ensembl.genes, keytype = "GENEID", columns = c("SYMBOL","GENEID"))
+
+# 2. Convert from gene.symbol to ensembl.gene
+geneSymbols <-  c('DDX26B','CCDC83',  'MAST3', 'RPL11', 'ZDHHC20',  'LUC7L3',  'SNORD49A',  'CTSH', 'ACOT8')
+
+geneIDs2 <- ensembldb::select(EnsDb.Hsapiens.v79, keys= geneSymbols, keytype = "SYMBOL", columns = c("SYMBOL","GENEID"))
